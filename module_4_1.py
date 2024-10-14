3c445a0e8c6e90fb6d247bdd89f83c9d975f9e4b
Vladimir1mir <korneevdeath@yandex.ru>
2024-10-14 20:55:08 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+
+from Модули.fake_math import divide as f
+from Модули.true_math import divide as t
+
+result1 = f(69, 3)
+result2 = f(3, 0)
+result3 = t(49, 7)
+result4 = t(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
